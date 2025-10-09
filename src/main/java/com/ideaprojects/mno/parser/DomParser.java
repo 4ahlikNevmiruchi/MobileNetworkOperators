cf7fc1b85d89e06ad30ddb6b99667cd15b04483b
mond <viktor.yudakov.1104@gmail.com>
2025-10-09 12:22:05 +0000

    DomParser visual and consistency fix

--- src/main/java/com/ideaprojects/mno/parser/DomParser.java
+++ src/main/java/com/ideaprojects/mno/parser/DomParser.java
@@ -10,36 +10,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DomParser {
+    private static final String NS = "http://com.ideaprojects.mno";
+
     public static List<Tariff> parse(File xmlFile) throws Exception {
         List<Tariff> list = new ArrayList<>();
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(xmlFile);
-        NodeList tariffs = doc.getElementsByTagNameNS("http://com.ideaprojects.mno","tariff");
+        NodeList tariffs = doc.getElementsByTagNameNS(NS, "tariff");
         for (int i=0;i<tariffs.getLength();i++){
             Element tEl = (Element) tariffs.item(i);
             Tariff t = new Tariff(tEl.getAttribute("id"));
-            t.setName(getText(tEl,"name"));
-            t.setOperatorName(getText(tEl,"operatorName"));
-            t.setPayroll(new BigDecimal(getText(tEl,"payroll")));
+            t.setName(getTextNS(tEl, "name"));
+            t.setOperatorName(getTextNS(tEl, "operatorName"));
+            t.setPayroll(new BigDecimal(getTextNS(tEl, "payroll")));
             CallPrices cp = new CallPrices();
-            Element cpEl = (Element) tEl.getElementsByTagName("callPrices").item(0);
-            cp.setInsideNetwork(new BigDecimal(getText(cpEl,"insideNetwork")));
-            cp.setOutsideNetwork(new BigDecimal(getText(cpEl,"outsideNetwork")));
-            cp.setLandline(new BigDecimal(getText(cpEl,"landline")));
+            Element cpEl = (Element) tEl.getElementsByTagNameNS(NS, "callPrices").item(0);
+            cp.setInsideNetwork(new BigDecimal(getTextNS(cpEl, "insideNetwork")));
+            cp.setOutsideNetwork(new BigDecimal(getTextNS(cpEl, "outsideNetwork")));
+            cp.setLandline(new BigDecimal(getTextNS(cpEl, "landline")));
             t.setCallPrices(cp);
-            t.setSmsPrice(new BigDecimal(getText(tEl,"smsPrice")));
+            t.setSmsPrice(new BigDecimal(getTextNS(tEl, "smsPrice")));
             // parameters
-            NodeList params = tEl.getElementsByTagName("parameter");
+            NodeList params = tEl.getElementsByTagNameNS(NS, "parameter");
             for (int j=0;j<params.getLength();j++){
                 Element pEl = (Element) params.item(j);
                 Parameter p = new Parameter();
                 p.setNameAttr(pEl.getAttribute("name"));
-                String fav = getText(pEl,"favoriteNumberCount");
+                String fav = getTextNS(pEl, "favoriteNumberCount");
                 if (fav!=null && !fav.isEmpty()) p.setFavoriteNumberCount(Integer.valueOf(fav));
-                p.setTariffization(getText(pEl,"tariffization"));
-                p.setActivationFee(new BigDecimal(getText(pEl,"activationFee")));
+                p.setTariffization(getTextNS(pEl, "tariffization"));
+                p.setActivationFee(new BigDecimal(getTextNS(pEl, "activationFee")));
                 t.addParameter(p);
             }
             list.add(t);
@@ -47,8 +49,8 @@ public class DomParser {
         return list;
     }
 
-    private static String getText(Element parent, String tagName){
-        NodeList nl = parent.getElementsByTagName(tagName);
+    private static String getTextNS(Element parent, String localName){
+        NodeList nl = parent.getElementsByTagNameNS(NS, localName);
         if(nl==null || nl.getLength()==0) return "";
         return nl.item(0).getTextContent().trim();
     }
